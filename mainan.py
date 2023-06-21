6935c352cfd099b4179117821e9b9da5fcc1671e
MSI CYBORG 15 <ekobudi301@gmail.com>
2023-06-21 17:41:22 +0700

    ay Paython Code

---
+++ mainan.py
@@ -0,0 +1,13 @@
+"""
+Semua sintaksis dasar bahasa pemrogaman terdiri dari
+1. Sekuensial : langkah Berurutan
+2. Percabangan : langkah Melompat jika Kondisi Memenuhi
+3. Perulangan : mengulang langkah tang sama berkali-kali selama/sampai kondisi terpenuhi
+"""
+
+# Sekuensial
+print("ibu berkata, ayah pengen ")
+print (" budi menjawab, baik,apa yang harus saya lakukan di toko ? ")
+print (" ibu menjawab, Beli satu botol susu, dan jika ada telor beli 6")
+print (" maka Budi berangkat ke toko ")
+print (" dan mulai berbelanja ")
