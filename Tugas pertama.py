cb475d4cb7165b6047a1d2810f8b42bfe4e2b9a5
MSI CYBORG 15 <ekobudi301@gmail.com>
2023-06-22 19:05:42 +0700

    Tugas

---
+++ Tugas pertama.py
@@ -0,0 +1,13 @@
+# TUGAS PERTAMA
+print(" mulai ")
+print (" ibu memberi perintah beli 1 botol susu ' ? ")
+print (" anak : Anak Menjawab ' OK ' ")
+print (" anak pergi ketoko ")
+print (" anak : anak mengecek tidak ada susu ditoko ")
+print (" ibu  : bertanya kembali apakah diwarung ada 6 butir telur  ")
+print (" anak menjawab ada  ")
+print (" ibu  : belikan 6 butir teluar aja untuk susunya di censel aja  ")
+print (" Anak Pulang Ke rumah  ")
+print (" Menyerahkan Hasil Belanja Kepada Ibu ")
+print (" Selesai  ")
+
