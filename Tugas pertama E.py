fe5b1f25b9363458a379e6f529893595e9b7e8cb
MSI CYBORG 15 <ekobudi301@gmail.com>
2023-06-25 10:05:17 +0700

    Tugas ulang

--- Tugas pertama E.py
+++ Tugas pertama E.py
@@ -1,5 +1,5 @@
 # TUGAS PERTAMA
-print(" mulai ")
+print (" mulai ")
 print (" ibu memberi perintah beli 1 botol susu ' ? ")
 print (" anak : Anak Menjawab ' OK ' ")
 print (" anak pergi ketoko ")
@@ -11,4 +11,17 @@ print (" Anak Pulang Ke rumah  ")
 print (" Menyerahkan Hasil Belanja Kepada Ibu ")
 print (" Selesai  ")
 
+# Percabangan
+jumlah_botol_susu = 6
+jumlah_telur = 1
+print (f"jumlah botol susu {jumlah_botol_susu} btl")
+print (f"jumlah Telur {jumlah_telur} btl")
 
+if jumlah_botol_susu > 0:
+    print (" budi mengecek harganya, dan ternyata uangnya cukup ")
+    if jumlah_telur == 0:
+        print (" Budi Membeli 1 botol susu ")
+    else:
+        print (" budi membeli 6 botol susu ")
+else:
+    print (" budi tidak jadi memebeli 1 botol susu ")
