e6b8f4e3ef0a31fe2a655233d125cd15aa55c540
MSI CYBORG 15 <ekobudi301@gmail.com>
2023-06-27 16:20:06 +0700

    Perulangan dengan wile dengan jumalah tak terbatas

--- Whele Undafend tugas ke 2 perulangan baca buku .py
+++ Whele Undafend tugas ke 2 perulangan baca buku .py
@@ -1,27 +1,27 @@
 """
 Program Perulangan membaca buku dengan while Sampai paham
 """
-jumlah_buku = 10
+buku = 10
 
-total_jumlah_baca = 10
+total_jumlah = 10
 
 print(" ibu berkata, ' baca semua bukumu ")
 
-jumlah_buku_yang_sudah_dibaca_dan_dipahami = 0
-print(f"jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}")
+jumlah_dipahami = 0
+print(f"jumlah buku yang sudah dibaca dan dipahami {jumlah_dipahami}")
 
 print(" Dengan while ")
-while total_jumlah_baca < jumlah_buku * 2:
-    total_jumlah_baca = total_jumlah_baca + 1
-    if jumlah_buku_yang_sudah_dibaca_dan_dipahami == 9:
-        print(f"buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1} belum paham")
+while total_jumlah < buku * 2:
+    total_jumlah = total_jumlah + 1
+    if jumlah_dipahami == 6:
+        print(f"buku ke {jumlah_dipahami + 1} belum paham")
     else:
-        jumlah_buku_yang_sudah_dibaca_dan_dipahami = jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1
-        print(f" buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami} sudah dibaca dan dipahami ")
+        jumlah_dipahami = jumlah_dipahami + 1
+        print(f" buku ke {jumlah_dipahami} sudah dibaca dan dipahami ")
 
-print(f"jumalh buku yang sudah dibaca dan dipahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}")
-if jumlah_buku_yang_sudah_dibaca_dan_dipahami == jumlah_buku :
+print(f"jumalh buku yang sudah dibaca dan dipahami {jumlah_dipahami}")
+if jumlah_dipahami == buku :
     print (" bu, semua buku  sudah dibaca dan dipahami ")
 else :
     print (f" bu, tidak semua buku bisa dipahami."
-           f" budi hanya bisa memahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami} buku ")
+           f" budi hanya bisa memahami {jumlah_dipahami} buku ")
